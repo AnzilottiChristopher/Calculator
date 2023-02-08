8730b2da58006daaf95bb1ea2c60c0500296c228
AnzilottiChristopher <anzilottichr@gmail.com>
2023-02-08 17:17:21 -0500

    Input two integers and adds and returns value

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,20 @@
+/*
+ * Chris Anzilotti
+ * Calculator Program
+ * 2/8/2023
+ */
+
+ import java.util.Scanner;
 public class Calculator
 {
     public static void main(String[] args) throws Exception 
     {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter two integers: ");
+        int firstNum = scan.nextInt();
+        int secondNum = scan.nextInt();
+        int finalNum = secondNum + firstNum;
+        System.out.printf("The sum is %d", finalNum);
+        scan.close();
     }
 }
