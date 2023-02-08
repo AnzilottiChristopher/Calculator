c2f0357edfa6a89ccbe9cfbea723e273812bd41e
AnzilottiChristopher <anzilottichr@gmail.com>
2023-02-08 18:13:46 -0500

    Input and answers are doubles

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,39 +9,38 @@ public class Calculator
 {
     public static void main(String[] args) throws Exception 
     {
-        //Global Variables
-        int finalVal;
-
         Scanner scan = new Scanner(System.in);
         //Asking for operation
         System.out.println("Enter a number for either Addition, Subtraction, Multiplication, Division: ");
-        System.out.println("1.Addition 2.Subtraction 3.Multiplication 4.Divisoin");
+        System.out.println("1.Addition 2.Subtraction 3.Multiplication 4.Division");
         int check = scan.nextInt();
 
         //Getting integer values
         System.out.println("Enter two integers: ");
-        int firstNum = scan.nextInt();
-        int secondNum = scan.nextInt();
+        double firstNum = scan.nextDouble();
+        double secondNum = scan.nextDouble();
 
+        //Doing the selected
+        double finalVal;
         if(check == 1)
         {
             finalVal = firstNum + secondNum;
-            System.out.printf("The sum is %d", finalVal);
+            System.out.printf("The sum is %f", finalVal);
         }
         if(check == 2)
         {
             finalVal = firstNum - secondNum;
-            System.out.printf("The difference is %d", finalVal);
+            System.out.printf("The difference is %f", finalVal);
         }
         if(check == 3)
         {
             finalVal = firstNum * secondNum;
-            System.out.printf("The product is %d", finalVal);
+            System.out.printf("The product is %f", finalVal);
         }
         if(check == 4)
         {
             finalVal = firstNum / secondNum;
-            System.out.printf("The quotient is %d", finalVal);
+            System.out.printf("The quotient is %f", finalVal);
         }
 
         
