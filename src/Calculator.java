d699718d4dff1e61a8ef638a1cfa142668a59577
AnzilottiChristopher <anzilottichr@gmail.com>
2023-02-08 18:08:37 -0500

    Multiplication and Division Added

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,13 +10,12 @@ public class Calculator
     public static void main(String[] args) throws Exception 
     {
         //Global Variables
-        int addition;
-        int subtraction;
+        int finalVal;
 
         Scanner scan = new Scanner(System.in);
         //Asking for operation
-        System.out.println("Enter a number for either Addition or Subtraction: ");
-        System.out.println("1. Addition  2. Subtraction");
+        System.out.println("Enter a number for either Addition, Subtraction, Multiplication, Division: ");
+        System.out.println("1.Addition 2.Subtraction 3.Multiplication 4.Divisoin");
         int check = scan.nextInt();
 
         //Getting integer values
@@ -26,13 +25,23 @@ public class Calculator
 
         if(check == 1)
         {
-            addition = firstNum + secondNum;
-            System.out.printf("The sum is %d", addition);
+            finalVal = firstNum + secondNum;
+            System.out.printf("The sum is %d", finalVal);
         }
         if(check == 2)
         {
-            subtraction = firstNum - secondNum;
-            System.out.printf("The difference is %d", subtraction);
+            finalVal = firstNum - secondNum;
+            System.out.printf("The difference is %d", finalVal);
+        }
+        if(check == 3)
+        {
+            finalVal = firstNum * secondNum;
+            System.out.printf("The product is %d", finalVal);
+        }
+        if(check == 4)
+        {
+            finalVal = firstNum / secondNum;
+            System.out.printf("The quotient is %d", finalVal);
         }
 
         
